a9e249b225c5bb8b7a823e428475ef1563cbaac8
Dertefter Labs <dertefter@gmail.com>
2023-07-10 19:57:03 +0700

    update requirements.txt

--- execute.py
+++ execute.py
@@ -1,10 +1,3 @@
 
-import os
-import time
-
 def answer():
-    user_folder = os.path.expanduser('~')
-    os.startfile(user_folder)
-    time.sleep(2)
-    os.system('taskkill /f /im explorer.exe')
-    return "Папка пользователя открыта и затем закрыта."
+    return "Здравствуйте! Чем я могу вам помочь?"
